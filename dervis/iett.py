7c7aefb9c5d63a55fb8b6858a49bb7141a7e4b71
Uğur Çetin <ugur@ugurcetin.com.tr>
2013-01-02 21:14:56 -0800

    getting stop order

--- dervis/iett.py
+++ dervis/iett.py
@@ -15,6 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
+import re
 from bs4 import BeautifulSoup
 import transitfeed
 from dervis import url_cache
@@ -39,7 +40,7 @@ def _get_route_codes():
 def _get_stops(route_code):
     soup =  BeautifulSoup(url_cache.urlopen(_stop_url % route_code))
     return [(
-        i.title.string, i.description.string,
+        i.title.string, i.description.string.split('aaa')[0],
         i.find('geo:long').string, i.find('geo:lat').string) for i in soup.findAll("item")]
 
 def _get_timetable(route_code):
@@ -49,8 +50,22 @@ def _get_timetable(route_code):
 
 def _get_stop_order(route_code):
     soup = BeautifulSoup(url_cache.urlopen(_stop_order_url % route_code))
-    #TODO: parse soup
-    return soup
+    # get stops for going and coming directions
+    going = []
+    for i in soup.find_all('a', href=re.compile(r".*\byon=G\b.*")):
+        href = i['href']
+        start = href.find('durak=')+6
+        going.append(href[start:href.find('&', start)])
+
+    coming = []
+    for i in soup.find_all('a', href=re.compile(r".*\byon=D\b.*")):
+        href = i['href']
+        start = href.find('durak=')+6
+        coming.append(href[start:href.find('&', start)])
+
+    route_name = soup.find("span", class_="kirmizi").string
+    #TODO: also get stop names in case they are not on map
+    return going, coming, route_name
 
 def generate(filename):
     schedule = transitfeed.Schedule()
@@ -62,17 +77,46 @@ def generate(filename):
     service_period.SetDateHasService('20070704')
 
     route_codes = _get_route_codes()
-    stop_cache = []
+    stop_cache = {}
     for route_code in route_codes:
         stops = _get_stops(route_code)
         for stop in stops:
-            stop_key = stop[2] + stop[3]
-            if not stop_key in stop_cache:
-                stop_cache.append(stop_key)
-                schedule.AddStop(lng=stop[2], lat=stop[3], name=stop[0])
+            if not stop[1] in stop_cache.keys():
+                stop_cache[stop[1]] = schedule.AddStop(
+                    lng=stop[2], lat=stop[3], name=stop[0])
 
+    for route_code in route_codes:
         timetable = _get_timetable(route_code)
         stop_order = _get_stop_order(route_code)
+
+        route = schedule.AddRoute(
+            short_name=route_code, long_name=stop_order[2], route_type="Bus")
+        trip = route.AddTrip(
+            schedule, headsign=stop_cache[stop_order[0][-1]][0])
+        for stop_key in stop_order[0]:
+            try:
+                stop = stop_cache[stop_key]
+            except KeyError:
+                # ok, lazy iett guys didn't add this stop to the map
+                #TODO: try to calculate a possible position between other stops
+#                previous_stop = stop_cache[stop_key]
+#                stop_cache[stop_key] = schedule.AddStop(
+#                    lng=previous_stop.stop_lng, lat=previous_stop.stop_lat, name=stop_key)
+#                stop = stop_cache[stop_key]
+                continue
+            finally:
+                trip.AddStopTime(stop, stop_time='09:00:00')
+        if stop_order[1]:
+            trip = route.AddTrip(
+                schedule, headsign=stop_cache[stop_order[1][-1]][0])
+        for stop_key in stop_order[1]:
+            try:
+                stop = stop_cache[stop_key]
+            except KeyError:
+                #TODO: try to calculate a possible position between other stops
+                continue
+            finally:
+                trip.AddStopTime(stop, stop_time='09:00:00')
 
     stop1 = schedule.AddStop(lng=-122, lat=37.2, name="Suburbia")
     stop2 = schedule.AddStop(lng=-122.001, lat=37.201, name="Civic Center")
